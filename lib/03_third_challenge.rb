10dcf72aa717ebe02b69aeccd79014d4b1c863af
Kristian Portillo <portillk@gmail.com>
2020-07-07 21:07:30 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -16,10 +16,10 @@ def third_challenge
       :patriarch => { :name, :age, :status
         #your key/value pairs here
         },
-      :matriarch => {
+      :matriarch => {:name, :age, :status
         #your key/value pairs here
         },
-      :heroine => {
+      :heroine => {:name, :age, :status
         #your key/value pairs here
         },
       :heroine_friends => []
