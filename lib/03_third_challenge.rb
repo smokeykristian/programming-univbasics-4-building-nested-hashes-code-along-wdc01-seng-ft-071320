0c9f948389927500a6d686d67dac674a2af41052
Kristian Portillo <portillk@gmail.com>
2020-07-07 21:21:41 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -7,8 +7,8 @@ def third_challenge
       :matriarch => {:name => "Lady Montague", 
       :age => "54", 
         },
-      :hero => {:name => "Romeo", :
-      age => "15", 
+      :hero => {:name => "Romeo", 
+      :age => "15", 
       :status => "Alive"
         },
    }, 
