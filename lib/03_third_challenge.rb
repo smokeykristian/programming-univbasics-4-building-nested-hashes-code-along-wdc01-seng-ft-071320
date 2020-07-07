1d7b54d2b0dbc3c2ee47b7c263ec6c4c01fe5879
Kristian Portillo <portillk@gmail.com>
2020-07-07 21:18:51 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -3,28 +3,25 @@ def third_challenge
    :montague => {
       :patriarch => {:name => "Lord Montague", 
       :age => "53", 
-      :status
         },
       :matriarch => {:name => "Lady Montague", 
       :age => "54", 
-      :status
         },
       :hero => {:name => "Romeo", :
       age => "15", 
       :status => "Alive"
-        #your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => { :name => "Lord Montague",
       :age => "50", 
-      :status
+      
         #your key/value pairs here
         },
       :matriarch => {:name => "Lady Montague", 
       :age => "51", 
-      :status
+      
         #your key/value pairs here
         },
       :heroine => {:name => "Juliet", 
